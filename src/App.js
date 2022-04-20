332f7ad87845f558c5e284ad827ac3ffbbc8c5f4
ELISA DANILO <elisadanilo2@gmail.com>
2022-04-20 17:10:15 +0200

    creation of the navbar with logo and home and map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import "./components/Navbar"
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      Bienvenue groupe 404 ! HOOO
+      <Navbar />
       <Routes>
         <Route />
       </Routes>
