0b6edd1404c1e27e57bce7242ec0eab4c727d931
Thomas K <thmskffr@gmail.com>
2022-04-20 14:31:36 +0200

    basic MobileHeader style, no routing, imported writing-white icon + wall-icon-white into asset/icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import MobileFooter from "./components/MobileFooter";
+import MobileHeader from "./components/MobileHeader";
 
 function App() {
   return (
     <div className="App flex flex-col h-screen">
+      <MobileHeader />
       <MobileFooter/>
       <Routes>
         <Route />
