60bf491c9781c45968732b1952314a3c82e1227f
ACCIPE-RABOT MICKAEL <mickael.accipe.rabot@gmail.com>
2022-04-21 10:10:00 +0200

    add comments form and comments list with like counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { CommentList } from "./components/comments/CommentList";
+
 
 function App() {
   return (
     <div className="App">
       Bienvenue groupe 404 ! HOOO
+      <CommentList/>
       <Routes>
         <Route />
       </Routes>
