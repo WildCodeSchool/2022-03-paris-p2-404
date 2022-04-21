4ea0ea502b829ebfb407bbee9e2b96cf4cc72065
Thomas K <thmskffr@gmail.com>
2022-04-21 09:21:34 +0200

    API fetch ok

--- src/components/ProfileInfoCard.jsx
+++ src/components/ProfileInfoCard.jsx
@@ -11,23 +11,12 @@ useEffect(() => {
     .then(data => setProfiles(data));
     },[]);
 
-useEffect(() => {
-    axios
-    .get()
-})
 console.log(profiles);
 
     return(
         <div>
           <img className="rounded-full border-4 border-black" src={profiles.imageUrl} alt="profile portrait" />
           <div className="isConnected bg-green-700 h-7 w-7 rounded-full"></div>
-          <div>Id : {profiles.id}</div>
-          <div>First Name : {profiles.firstName}</div>
-          <div>Last Name : {profiles.lastName}</div>
-          <div>Full Name : {profiles.fullName}</div>
-          <div>Title : {profiles.title}</div>
-          <div>Family : {profiles.family}</div>
-          <div>Image File : {profiles.image}</div>
         </div>
     );
 };
