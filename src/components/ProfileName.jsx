e7cd25ecba8afa330d4c33a33c6ed6b1fb6839df
Thomas K <thmskffr@gmail.com>
2022-04-26 14:59:54 +0200

    functionnal info display based on parent's props (from thronesapi)

---
+++ src/components/ProfileName.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import GroupsIcon from '@mui/icons-material/Groups';
+import PersonIcon from '@mui/icons-material/Person';
+import BadgeIcon from '@mui/icons-material/Badge';
+
+function ProfileName(props) {
+
+  const {id, fullName, title, family} = props.profiles;
+  
+  return (
+    <div>
+      <div className="flex ">
+        <PersonIcon></PersonIcon>
+        <h2>{fullName}</h2>
+      </div>
+      <div className="flex ">
+        <BadgeIcon></BadgeIcon>
+        <h3>{title}</h3>
+      </div>
+      <div className="flex ">
+        <GroupsIcon></GroupsIcon>
+        <h3>{family}</h3>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileName;
