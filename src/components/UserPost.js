d9f0785014f40b38fc93f682a4ddf010d1573831
ACCIPE-RABOT MICKAEL <mickael.accipe.rabot@gmail.com>
2022-04-28 10:09:01 +0200

    change style on hr(size) and picture(shadow) in userPost

--- src/components/UserPost.js
+++ src/components/UserPost.js
@@ -25,11 +25,11 @@ export const UserPost = () => {
   let randomId = Math.floor(Math.random() * 53);
 
   return (
-    <div className="UserPost bg-color-winter-primary w-full my-4 rounded-lg text-color-black shadow-color-font-dark shadow-md">
+    <div className="UserPost relative bg-color-winter-primary w-full my-4 rounded-lg text-color-black shadow-color-font-dark shadow-md">
       {/* md:w-3/5 */}
       <div className="postHeader  flex flex-row gap-x-4 pt-4 pl-4 pb-4">
         {/* Display profile picture */}
-        <div className="picContent w-2/12">
+        <div className="z-50 picContent w-2/12">
           <div className="postPicture absolute">
             <PostPicture character={character} />
           </div>
@@ -39,7 +39,7 @@ export const UserPost = () => {
           <UserInfo character={character} />
         </div>
       </div>
-      <hr className="border-1 border-black" />
+      <hr className="w-10/12 absolute right-0 border-1 border-black" />
       <div className="postContent px-10">
         {/* Display post content */}
         <UserQuote character={character} />
