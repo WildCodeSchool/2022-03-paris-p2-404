a233a98eccdd1e7a3ff2d38b3b53d795ca2075ed
ACCIPE-RABOT MICKAEL <mickael.accipe.rabot@gmail.com>
2022-04-28 09:34:01 +0200

    notif cards routed with Link (url hardcoded) to profile pages

--- src/components/NotifDaenerysCard.js
+++ src/components/NotifDaenerysCard.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NotifDaenerysCard = ( {daenerys, date} ) => {
+const NotifDaenerysCard = ({ daenerys, date }) => {
   return (
-<div className=''>
-<ul className='mx-2 shadow-color-font-dark shadow-md rounded-2xl '>
-    <li className='flex flex-row justify-between items-center bg-color-winter-primary border-solid border rounded-2xl border-black	'>
-        <img src={daenerys.imageUrl} alt={daenerys.firstName} className="rounded-full object-cover h-12 w-12 m-2" />
-        <p className=''> <span className='font-bold'>{daenerys.firstName}</span> liked your profile photo</p>    <p className='mx-4'>{date}</p>
-    </li> 
-</ul>
-</div>
-)};
+    <div className="">
+      <ul className="mx-2 shadow-color-font-dark shadow-md rounded-2xl ">
+        <li className="flex flex-row justify-between items-center bg-color-winter-primary border-solid border rounded-2xl border-black	">
+          <Link to={`/users/0`}>
+            <img
+              src={daenerys.imageUrl}
+              alt={daenerys.firstName}
+              className="rounded-full object-cover h-12 w-12 m-2"
+            />
+          </Link>
+          <p className="">
+            {' '}
+            <span className="font-bold">{daenerys.firstName}</span> liked your
+            profile photo
+          </p>{' '}
+          <p className="mx-4">{date}</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-
-
-export default NotifDaenerysCard
+export default NotifDaenerysCard;
--- src/components/NotifRamsayCard.js
+++ src/components/NotifRamsayCard.js
@@ -1,19 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NotifRamsayCard = ( {ramsay, date} ) => {
+const NotifRamsayCard = ({ ramsay, date }) => {
   return (
-<div >
-<ul className='mx-2  rounded-2xl'>
-    <li className='flex flex-row justify-between items-center bg-color-winter-primary border-solid border rounded-2xl border-black mb-4 shadow-color-font-dark shadow-md	'>
-        <img src={ramsay.imageUrl} alt={ramsay.firstName} className="rounded-full object-cover h-12 w-12 m-2" />
-        <p className='text-center'> <span className='font-bold'>{ramsay.firstName}</span> send you a message "You are a bastard !" </p> <p className='mx-4'>{date}</p>
-    </li> 
-    <li className='flex flex-row items-center justify-between bg-color-winter-primary border-solid border rounded-2xl border-black shadow-color-font-dark shadow-md		'>
-        <img src={ramsay.imageUrl} alt={ramsay.firstName} className="rounded-full object-cover h-12 w-12 m-2" />
-        <p> <span className='font-bold'>{ramsay.firstName}</span>  send you a duel request </p> <p className='mx-4 '>{date}</p> 
-    </li> 
-</ul>
-</div>
-)};
+    <div>
+      <ul className="mx-2  rounded-2xl">
+        <li className="flex flex-row justify-between items-center bg-color-winter-primary border-solid border rounded-2xl border-black mb-4 shadow-color-font-dark shadow-md	">
+        <Link to={`/users/35`}>
+            <img
+            src={ramsay.imageUrl}
+            alt={ramsay.firstName}
+            className="rounded-full object-cover h-12 w-12 m-2"
+          />
+          </Link>
+          <p className="text-center">
+            {' '}
+            <span className="font-bold">{ramsay.firstName}</span> send you a
+            message "You are a bastard !"{' '}
+          </p>{' '}
+          <p className="mx-4">{date}</p>
+        </li>
+        <li className="flex flex-row items-center justify-between bg-color-winter-primary border-solid border rounded-2xl border-black shadow-color-font-dark shadow-md		">
+        <Link to={`/users/35`}>
+          <img
+            src={ramsay.imageUrl}
+            alt={ramsay.firstName}
+            className="rounded-full object-cover h-12 w-12 m-2"
+          />
+          </Link>
+          <p>
+            {' '}
+            <span className="font-bold">{ramsay.firstName}</span> send you a
+            duel request{' '}
+          </p>{' '}
+          <p className="mx-4 ">{date}</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default NotifRamsayCard
+export default NotifRamsayCard;
