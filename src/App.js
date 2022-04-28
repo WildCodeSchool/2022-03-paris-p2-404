aef4321786a5307d0bce28073d0eed7832a246d0
Thomas K <thmskffr@gmail.com>
2022-04-28 17:23:40 +0200

    functionnal routing in MobileFooter with NavLinks

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
         <Route path="/latest" element={< NotifUserList />} />
         {/* <Route path="/map" element={<Map />} /> */}
       </Routes>
+      <MobileFooter />
     </div>
   );
 }
