54d090b270bb87bf5d042ecb3921f27059e0a5d1
ELISA DANILO <elisadanilo2@gmail.com>
2022-05-03 10:24:47 +0200

    create a file post form, need a express server to work

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
   
   return (
     <div className="App">
+      <MobileHeader />
       <Routes>
         <Route path="/" element={< HomePage />} />
         {/* <Route path="/login" element={< LoginPage />} /> */}
