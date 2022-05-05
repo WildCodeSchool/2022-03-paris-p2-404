c02937e35be08ad7b020e0547d0be3196b3c32d4
Guillaume <miche.guillaume@isitparis.eu>
2022-05-05 15:34:44 +0200

    responsive profile page

--- src/components/ProfileInfoCard.jsx
+++ src/components/ProfileInfoCard.jsx
@@ -7,53 +7,62 @@ import Navbar from "./Navbar";
 import { CoverPictures } from "./coverPictures/CoverPictures";
 import Profile_Interact_Button from "./Profile_Interact_Button";
 import { UserPost } from "./UserPost";
-import throne from "../assets/icons/throne.png"
+import throne from "../assets/icons/throne.png";
 import { useParams } from "react-router-dom";
 
 function ProfileInfoCard() {
-    const [profiles, setProfiles] = useState({});
+  const [profiles, setProfiles] = useState({});
 
-    let { id } = useParams();
+  let { id } = useParams();
 
-    useEffect(() => {
-        axios
-            .get("https://thronesapi.com/api/v2/Characters/" + id)
-            .then(res => res.data)
-            .then(data => setProfiles(data));
-    }, [id]);
+  useEffect(() => {
+    axios
+      .get("https://thronesapi.com/api/v2/Characters/" + id)
+      .then((res) => res.data)
+      .then((data) => setProfiles(data));
+  }, [id]);
 
-    console.log(profiles);
+  console.log(profiles);
 
-    return (
-        <div>
-            <Navbar />
-            <div className="profilecontent flex flex-col items-center ">
-                <div className="relative flex flex-col items-center">
-                    <CoverPictures profiles={profiles} />
-                    <div className="absolute -bottom-24">
-                        <ProfilePicture profiles={profiles} />
-                    </div>
-                </div>
-                <div className="flex w-screen">
-                    <div className="w-1/2">
-                    <div className=" absolute flex my-4">
-                    <Profile_Interact_Button />
-                    </div>
-                    </div>
-                    <div className="flex w-1/2 justify-center h-32">
-                    </div>
-                </div>
-                <div className="flex flex-col items-center w-screen mb-8 border-2 p-2 customshadow">
-                    <ProfileName profiles={profiles} />
-                    <ProfileQuote profiles={profiles} />
-                </div>
-                <div className="w-4/6">
-                    <div>
-                      {Array.apply(null, {length: ((Math.random()*30)+1)}).map(item => <UserPost />)}
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <Navbar />
+      <div className="profilecontent flex flex-col items-center ">
+        <div className="relative flex flex-col items-center">
+          <CoverPictures profiles={profiles} />
+          <div className="absolute -bottom-24">
+            <ProfilePicture profiles={profiles} />
+          </div>
         </div>
-    );
-};
+        <div className="flex w-screen">
+          <div className="w-1/2">
+            <div className=" absolute flex my-4">
+              <Profile_Interact_Button />
+            </div>
+          </div>
+          <div className="flex w-1/2 justify-center h-32"></div>
+        </div>
+        <div className="flex flex-col items-center w-screen mb-8 border-2 p-2 customshadow">
+          <ProfileName profiles={profiles} />
+          <ProfileQuote profiles={profiles} />
+        </div>
+        <div
+          className="
+            flex flex-col justify-center items-center
+            lg:w-8/10 w-full
+            px-4
+        "
+        >
+          <div className="w-full lg:w-8/12">
+            {Array.apply(null, { length: Math.random() * 30 + 1 }).map(
+              (item) => (
+                <UserPost />
+              )
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default ProfileInfoCard;
