bc836315f65edd69e5fe6fdeeb16a10ac1dc6349
ACCIPE-RABOT MICKAEL <mickael.accipe.rabot@gmail.com>
2022-05-05 09:26:02 +0200

    new css for map page

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,11 +1,14 @@
 import { Routes } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import React, { useState } from 'react';
-import thegreatnorthmap from '../assets/img/thegreatnorthmap.png';
-import thenorthmap from '../assets/img/thenorthmap.png';
-import map_mountains from '../assets/img/map_mountains.png';
-import map_rivers from '../assets/img/map_rivers.png';
-import map_rock from '../assets/img/map_rock.png';
+import PSdorne from '../assets/img/PSdorne.png';
+import PSvalemountain from '../assets/img/PSvalemountain.png';
+import PSgreatnorth from '../assets/img/PSgreatnorth.png';
+import PSironislands from '../assets/img/PSironislands.png';
+import PSnorth from '../assets/img/PSnorth.png';
+import PSreach from '../assets/img/PSreach.png';
+import PSstormlands from '../assets/img/PSstormlands.png';
+import PStherock from '../assets/img/PStherock.png';
 import compass from '../assets/icons/compass.png';
 import MobileFooter from '../components/MobileFooter';
 import MobileHeader from '../components/MobileHeader';
@@ -359,93 +362,98 @@ export const Map = () => {
 
       <div
         className="mapContent
-      grid grid-cols-2 grid-rows-9
+      grid grid-cols-2 
     m-4 lg:m-16 lg:mx-48 h-full items-center justify-center
       shadow-slate-700"
       >
-        <h2 className="countries m-4 text-center wrap lg:text-xl">The Great North</h2>
-        <div className=" m-2 mx-4 hover:scale-105 transition-all  ">
+        <h2 className="countries m-4 text-center wrap lg:text-2xl">The Great North</h2>
+        <div className=" mb-20 hover:scale-125 transition-all w-full  ">
           <img
             onClick={handleVisible}
             className="greatnorth  w-full h-full cursor-pointer transition-all"
             alt="#"
-            src={thegreatnorthmap}
+            src={PSgreatnorth}
           ></img>
         </div>
 
-        <h2 className="countries m-4 text-center lg:text-xl">The North</h2>
-        <div className=" m-2 mx-4 hover:scale-105  transition-all  ">
+        <h2 className="countries m-4 text-center lg:text-2xl">The North</h2>
+        <div className=" m-2 mx-4 hover:scale-125 transition-all w-full  ">
           <img
             onClick={handleVisible2}
             className="greatnorth  w-full h-full cursor-pointer"
-            src={thenorthmap}
+            src={PSnorth}
             alt="north"
           />
         </div>
 
-        
-
-        <h2 className="countries m-4 text-center lg:text-xl">Mountain and the vale</h2>
-        <div className=" m-2 mx-4 hover:scale-105 transition-all  ">
-          <img
-            onClick={handleVisible3}
-            className="greatnorth  w-full h-full cursor-pointer"
-            alt="#"
-            src={map_mountains}
-          ></img>
-        </div>
-        <h2 className="countries m-4 text-center lg:text-xl">Isles and the Rivers</h2>
-        <div className=" m-2 mx-4 hover:scale-105 transition-all  ">
+        <h2 className="countries m-4 text-center lg:text-2xl">Isles and the Rivers</h2>
+        <div className=" mr-24 hover:scale-125 transition-all w-full  ">
           <img
             onClick={handleVisible4}
             className="greatnorth  w-full h-full cursor-pointer"
             alt="#"
-            src={map_rivers}
+            src={PSironislands}
           ></img>
         </div>
-        <h2 className="countries m-4 text-center lg:text-xl">The rock</h2>
-        <div className=" m-2 mx-4 hover:scale-105 transition-all  ">
+
+        
+
+        <h2 className="countries m-4 text-center lg:text-2xl">Mountain and the vale</h2>
+        <div className=" ml-16 hover:scale-125 transition-all w-4/5  ">
+          <img
+            onClick={handleVisible3}
+            className="greatnorth  w-full h-full cursor-pointer"
+            alt="#"
+            src={PSvalemountain}
+          ></img>
+        </div>
+        
+        <h2 className="countries m-4 text-center lg:text-2xl">The rock</h2>
+        <div className=" mr-16 mt-20 hover:scale-125 transition-all w-full  ">
           <img
             onClick={handleVisible5}
             className="greatnorth  w-full h-full cursor-pointer"
             alt="#"
-            src={map_rock}
+            src={PStherock}
           ></img>
         </div>
-        <h2 className="countries m-4 text-center lg:text-xl">Stormlands</h2>
-        <div className=" m-2 mx-4 hover:scale-105 transition-all  ">
-          <img
-            onClick={handleVisible6}
-            className="greatnorth  w-full h-full cursor-pointer"
-            alt="#"
-            src={thegreatnorthmap}
-          ></img>
-        </div>
-        <h2 className="countries m-4 text-center lg:text-xl">The reach</h2>
-        <div className=" m-2 mx-4 hover:scale-105 transition-all  ">
+
+        <h2 className="countries m-4 text-center lg:text-2xl">The reach</h2>
+        <div className="  ml-12 hover:scale-125 transition-all w-full  ">
           <img
             onClick={handleVisible7}
             className="greatnorth  w-full h-full cursor-pointer"
             alt="#"
-            src={thegreatnorthmap}
+            src={PSreach}
           ></img>
         </div>
-        <h2 className="countries m-4 text-center lg:text-xl">Dorne</h2>
-        <div className=" m-2 mx-4 hover:scale-105 transition-all  ">
+
+        <h2 className="countries m-4 text-center lg:text-2xl">Stormlands</h2>
+        <div className=" m-2 mr-8 hover:scale-110 transition-all w-full  ">
           <img
-          onClick={handleVisible8}
+            onClick={handleVisible6}
             className="greatnorth  w-full h-full cursor-pointer"
             alt="#"
-            src={thegreatnorthmap}
+            src={PSstormlands}
           ></img>
         </div>
-        <h2 className="countries m-4 text-center lg:text-xl">King's Landing</h2>
-        <div className=" m-2 mx-4 hover:scale-105 transition-all  ">
+        
+        <h2 className="countries m-4 text-center lg:text-2xl">Dorne</h2>
+        <div className=" m-2 mt-16 hover:scale-150 transition-all w-4/5  ">
+          <img
+          onClick={handleVisible8}
+            className="greatnorth mt-8 w-full h-full cursor-pointer"
+            alt="#"
+            src={PSdorne}
+          ></img>
+        </div>
+        <h2 className="countries m-4 text-center lg:text-2xl">King's Landing</h2>
+        <div className=" hover:scale-125 transition-all w-full  ">
           <img
           onClick={handleVisible9}
             className="greatnorth  w-full h-full cursor-pointer"
             alt="#"
-            src={thegreatnorthmap}
+            src={PSnorth}
           ></img>
         </div>
       </div>
