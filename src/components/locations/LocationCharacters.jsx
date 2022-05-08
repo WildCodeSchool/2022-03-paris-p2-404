f02c4fb8625e04d3733932c2db7cb6d1a786c804
ACCIPE-RABOT MICKAEL <mickael.accipe.rabot@gmail.com>
2022-05-08 21:31:13 +0200

    end of map

--- src/components/locations/LocationCharacters.jsx
+++ src/components/locations/LocationCharacters.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import Profile_Interact_Button from '../Profile_Interact_Button';
+import { Link } from 'react-router-dom';
 
-export const LocationCharacters = ({ characters }) => {
+export const LocationCharacters = ({ characters}) => {
   console.log(characters);
   return (
     <div>
       <div className="flex flex-row items-center justify-between 
       gap-4 m-4 rounded-2xl
       bg-color-winter-primary shadow-color-font-dark shadow-md">
-        <img
+        <Link to={`/users/${characters.id}`}><img
           className="w-12 h-12 rounded-full m-2"
           src={characters.imageUrl}
           alt="#"
-        />
+        /></Link>
         <h3>{characters.fullName}</h3>
       </div>
     </div>
